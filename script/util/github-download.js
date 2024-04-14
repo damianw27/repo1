f5443b46b9ab181e1ae915a2e6ab1d4857d1e170
Damian Wileński <wilenskid.dev@gmail.com>
2024-04-14 20:06:23 +0200

    Update github-download.js

--- script/util/github-download.js
+++ script/util/github-download.js
@@ -32,25 +32,30 @@ const downloadFromGithub = async (path, repository) => {
   };
 
   console.log(`Attempt to download file data for '${path}' from '${repository}'`);
-  
-  const response = await axios({
-    url: `https://api.github.com/repos/${repository}/contents/${path}`,
-    method: 'get',
-    headers,
-  });
 
-  if (response.data.type === 'file') {
-    await githubDownload(response.data.download_url, response.data.name);
-  }
-
-  if (response.data.type === 'dir') {
-    fs.mkdirSync(`./tmp/${response.data.name}`);
-
-    const downloads = response.data.entries.map((entry) => {
-      return githubDownload(entry.download_url, `${response.data.name}/${entry.name}`);
+  try {
+    const response = await axios({
+      url: `https://api.github.com/repos/${repository}/contents/${path}`,
+      method: 'get',
+      headers,
     });
-
-    await Promise.all(downloads);
+  
+    if (response.data.type === 'file') {
+      await githubDownload(response.data.download_url, response.data.name);
+    }
+  
+    if (response.data.type === 'dir') {
+      fs.mkdirSync(`./tmp/${response.data.name}`);
+  
+      const downloads = response.data.entries.map((entry) => {
+        return githubDownload(entry.download_url, `${response.data.name}/${entry.name}`);
+      });
+  
+      await Promise.all(downloads);
+    }
+  } catch (error) {
+    console.error(`Error downloading file: ${error.message}`);
+    fs.unlinkSync(tmpFilePath);
   }
 };
 
