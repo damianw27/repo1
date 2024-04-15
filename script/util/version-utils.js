ef9b08c988b77b1e9a9098d8614afb71a594caf0
Damian Wileński <wilenskid.dev@gmail.com>
2024-04-15 10:21:56 +0200

    Update version-utils.js

--- script/util/version-utils.js
+++ script/util/version-utils.js
@@ -2,22 +2,9 @@ const packageJson = require('../../package.json');
 const path = require("path");
 const fs = require("fs");
 
-function readVersionFromTmpDir() {
-    const workingDir = path.join(__dirname, '..', '..');
-    const grammarVersionFile = path.join(workingDir, 'tmp', 'version.json');
-
-    if (!fs.existsSync(grammarVersionFile)) {
-        throw new Error('Grammar version file is not existing.')
-    }
-
-    const versionFileContent = fs.readFileSync(grammarVersionFile).toString('utf-8');
-    const parsedVersionFile = JSON.parse(versionFileContent);
-    return `${parsedVersionFile.major}.${parsedVersionFile.minor}.${parsedVersionFile.patch}`;
-}
-
 function getCurrentVersion() {
     const editorVersion = packageJson.version;
-    const grammarVersion = readVersionFromTmpDir();
+    const grammarVersion = packageJson.grammarVersion ?? 'null';
     return `${editorVersion}_${grammarVersion}`;
 }
 
